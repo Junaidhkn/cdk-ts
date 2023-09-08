25b1aacd7af7f29796d7ea98c3b03c4d123d89d0
junaidhkn <junaidhkniazi@gmail.com>
2023-09-09 00:42:18 +0500

    lambda funtion

--- src/infra/Launcher.ts
+++ src/infra/Launcher.ts
@@ -1,8 +1,8 @@
-import { App } from "aws-cdk-lib";
-import { DataStack } from "./stacks/DataStack";
+import { App } from 'aws-cdk-lib';
+import { DataStack } from './stacks/DataStack';
+import { LambdaStack } from './stacks/LmbdaStack';
 
+const app = new App();
 
-
-const app = new App()
-
-new DataStack(app,'DataStack')
+new DataStack(app, 'DataStack');
+new LambdaStack(app, 'LambdaStack');
