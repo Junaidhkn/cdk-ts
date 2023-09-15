71a758220308e6032ad86b01f55a8c7b7221d261
junaidhkn <junaidhkniazi@gmail.com>
2023-09-15 23:13:42 +0500

    cognito

--- src/infra/stacks/AuthStack.ts
+++ src/infra/stacks/AuthStack.ts
@@ -1,7 +1,11 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnOutput, Stack, StackProps } from 'aws-cdk-lib';
+import { UserPool, UserPoolClient } from 'aws-cdk-lib/aws-cognito';
 import { Construct } from 'constructs';
 
 export class AuthStack extends Stack {
+	private userPool: UserPool;
+	private userPoolClient: UserPoolClient;
+
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props);
 
@@ -9,7 +13,31 @@ export class AuthStack extends Stack {
 		this.createUserPoolClient();
 	}
 
-	private createUserPool() {}
+	private createUserPool() {
+		this.userPool = new UserPool(this, 'SpaceUserPool', {
+			selfSignUpEnabled: true,
+			signInAliases: {
+				email: true,
+				username: true,
+			},
+		});
+		new CfnOutput(this, 'SpaceUserPoolId', {
+			value: this.userPool.userPoolId,
+		});
+	}
 
-	private createUserPoolClient() {}
+	private createUserPoolClient() {
+		this.userPoolClient = this.userPool.addClient('SpaceUserPoolClient', {
+			authFlows: {
+				userPassword: true,
+				userSrp: true,
+				custom: true,
+				adminUserPassword: true,
+			},
+		});
+
+		new CfnOutput(this, 'SpaceUserPoolClientId', {
+			value: this.userPoolClient.userPoolClientId,
+		});
+	}
 }
