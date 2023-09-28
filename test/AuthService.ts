21a42370f8420510fea545ecc25051a1d5e9edb1
junaidhkn <junaidhkniazi@gmail.com>
2023-09-28 16:31:29 +0500

    amplify config

--- test/AuthService.ts
+++ test/AuthService.ts
@@ -1,5 +1,15 @@
-import { CognitoUser } from '@aws-amplify/auth';
-import { Auth } from 'aws-amplify';
+import { type CognitoUser } from '@aws-amplify/auth';
+import { Amplify, Auth } from 'aws-amplify';
+
+const awsRegion = 'ap-south-1';
+
+Amplify.configure({
+	Auth: {
+		region: awsRegion,
+		userPoolId: 'ap-south-1_DZ1I3LUBG',
+		userPoolWebClientId: '78778mm5kstcpc8a9krklv2dm8',
+	},
+});
 
 export class AuthService {
 	public async login(userName: string, password: string) {
